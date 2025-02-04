99bec373d4d05b0ddb7fd84e0d5fc44bd2fac8df
ELIOTT MURRAY <0murre89@solent.ac.uk>
2025-02-04 10:48:19 +0000

    Add testcases

--- person.py
+++ person.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self, name, weight, hunger, energy):
+    def __init__(self, name, weight, hunger, energy, age, age_look):
         if(hunger < 0 or hunger > 100):
             raise Exception("Hunger must be between 0 and 100")
         elif (energy < 0 or energy > 100):
@@ -9,6 +9,8 @@ class Person:
         self.weight = weight
         self.hunger = hunger
         self.energy = energy
+        self.age = age
+        self.age_look = age_look
  
     def rest(self):
         self.energy = self.energy+1
@@ -17,21 +19,16 @@ class Person:
     def run(self, miles):
         # For every mile, the person will lose/gain two units
         change = miles * 2
-        print(change)
 
         if self.energy-change > 0 and self.hunger+change <= 100:
             self.energy = self.energy-change
             self.hunger = self.hunger+change
             return True
         return False
-    
-    def print(self):        
-        print(self.energy)
 
-
-
-p1 = Person("abc", 150, 100, 10)
-p1.print()
-
-p1.run(2)
-p1.print()
+    def can_buy_alcohol(self):
+        if self.age_look > 25:
+            return True
+        elif self.age > 18:
+            return True
+        return False
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,20 +1,24 @@
 import pytest
-from person import Person
+import person as Person
 
 class TestPerson:
 
-    def test_rest_energy_changes(self):
-        p1 = Person("abc", 150, 99, 1)
-        print("Person is resting")
-        p1.rest()
-        assert p1.energy == 2
+    def test_rest_energy_changes(self, default_person_adult):
+        default_person_adult.rest()
+        assert default_person_adult.energy == 2
 
-    def test_rest_hunger_changes(self):
-        p1 = Person("abc", 150, 99, 1)
-        p1.rest()
-        assert p1.hunger == 100
+    def test_rest_hunger_changes(self, default_person_adult):
+        default_person_adult.rest()
+        assert default_person_adult.hunger == 51
 
-    def test_energy_changes(self):
-        p1 = Person("abc", 150, 100, 10)
-        p1.run(2)
-        assert p1.energy == 6
+    def test_energy_changes(self, default_person_adult):
+        default_person_adult.run(2)
+        assert default_person_adult.energy == 1
+
+    @pytest.mark.parametrize('age, age_look, expected_result', [(18,26,True),(17,26,True),(16,20,False)])
+    def test_age_look_ranges(self, default_person_adult, age, age_look, expected_result):
+        default_person_adult.age = age
+        default_person_adult.age_look = age_look
+
+        assert default_person_adult.can_buy_alcohol() == expected_result
+
